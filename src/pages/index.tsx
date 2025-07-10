c6bbadc8652d766d5520537580f236e0d5c3c94d
Jacobtread <jacobtread@gmail.com>
2025-07-11 01:08:12 +1200

    feat: better responsive design for homepage with mobile support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
             <Heading as="h1" className="hero__title">
               <span className={styles.srOnly}> {siteConfig.title}</span>
 
-              <img src="/logo.svg" />
+              <img src="/logo.svg" width={480} className={styles.logo} />
             </Heading>
 
             <p className={clsx("hero__subtitle", styles.heroSubtitle)}>
@@ -43,41 +43,51 @@ function HomepageHeader() {
             </div>
           </div>
 
-          <div>
-            <div className={styles.docArea}>
+          <div className={styles.heroRight}>
+            <div style={{ position: "relative", width: 300, height: 300 }}>
+              {/* Central icon (anchor) */}
+              <img
+                src="/document.svg"
+                width={92}
+                height={92}
+                style={{
+                  position: "absolute",
+                  left: 104,
+                  top: 104,
+                  zIndex: 3,
+                  transform: "rotate(-5deg)",
+                }}
+              />
+
+              {/* Top Left */}
               <img
                 src="/ppt.svg"
                 width={92}
                 height={92}
                 style={{
                   position: "absolute",
-                  left: 0,
-                  top: 0,
-                  transform: "rotate(-30deg)",
+                  left: 40,
+                  top: 15,
+                  zIndex: 2,
+                  transform: "rotate(-25deg)",
                 }}
               />
+
+              {/* Top Right */}
               <img
                 src="/pdf.svg"
                 width={92}
                 height={92}
                 style={{
                   position: "absolute",
-                  left: 55,
-                  top: 55,
-                  transform: "rotate(20deg)",
+                  left: 170,
+                  top: 15,
+                  zIndex: 2,
+                  transform: "rotate(15deg)",
                 }}
               />
-              <img
-                src="/txt.svg"
-                width={92}
-                height={92}
-                style={{
-                  position: "absolute",
-                  right: 0,
-                  top: 0,
-                  transform: "rotate(30deg)",
-                }}
-              />
+
+              {/* Bottom Left */}
               <img
                 src="/video.svg"
                 width={92}
@@ -85,42 +95,51 @@ function HomepageHeader() {
                 style={{
                   position: "absolute",
                   left: 50,
-                  top: 120,
-                  transform: "rotate(-45deg)",
+                  top: 190,
+                  zIndex: 1,
+                  transform: "rotate(-40deg)",
                 }}
               />
+
+              {/* Bottom Right */}
               <img
                 src="/xls.svg"
                 width={92}
                 height={92}
                 style={{
                   position: "absolute",
-                  left: 135,
-                  top: 75,
-                  transform: "rotate(-35deg)",
+                  left: 180,
+                  top: 190,
+                  zIndex: 1,
+                  transform: "rotate(25deg)",
                 }}
               />
+
+              {/* Middle Left */}
+              <img
+                src="/txt.svg"
+                width={92}
+                height={92}
+                style={{
+                  position: "absolute",
+                  left: 10,
+                  top: 100,
+                  zIndex: 2,
+                  transform: "rotate(-10deg)",
+                }}
+              />
+
+              {/* Middle Right */}
               <img
                 src="/image.svg"
                 width={92}
                 height={92}
                 style={{
                   position: "absolute",
-                  left: 150,
-                  top: 0,
-                  transform: "rotate(30deg)",
-                }}
-              />
-
-              <img
-                src="/document.svg"
-                width={92}
-                height={92}
-                style={{
-                  position: "absolute",
-                  left: 75,
-                  top: -50,
-                  transform: "rotate(-10deg)",
+                  left: 200,
+                  top: 90,
+                  zIndex: 2,
+                  transform: "rotate(10deg)",
                 }}
               />
             </div>
