c6114f3a1a54b9db87f6ab45efe41ebf6e385da1
jacobtread <jacobtread@gmail.com>
2026-07-14 08:15:28 +1200

    chore: switch to pnpm and update everything

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,145 +6,145 @@ import type * as OpenApiPlugin from "docusaurus-plugin-openapi-docs";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "Docbox",
-  tagline: "Multi-tenant file management, processing, and search platform",
-  favicon: "img/favicon.ico",
+    title: "Docbox",
+    tagline: "Multi-tenant file management, processing, and search platform",
+    favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: "https://docbox-nz.pages.dev",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+    // Set the production url of your site here
+    url: "https://docbox-nz.pages.dev",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "docbox-nz", // Usually your GitHub org/user name.
-  projectName: "docbox-website", // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "docbox-nz", // Usually your GitHub org/user name.
+    projectName: "docbox-website", // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
-
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          docItemComponent: "@theme/ApiItem", // Derived from docusaurus-theme-openapi
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
-        },
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
-
-  themeConfig: {
-    colorMode: {
-      defaultMode: "dark",
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
     },
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
-    navbar: {
-      logo: {
-        alt: "Docbox",
-        src: "logo-dark.svg",
-        srcDark: "logo-light.svg",
-        width: 120,
-      },
-      items: [
-        {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
-          position: "left",
-          label: "Docs",
-        },
-        {
-          type: "docSidebar",
-          sidebarId: "apiSidebar",
-          position: "left",
-          label: "API",
-        },
-        {
-          href: "https://github.com/docbox-nz",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
+
+    presets: [
+        [
+            "classic",
             {
-              label: "Tutorial",
-              to: "/docs/guides/intro",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "GitHub",
-              href: "https://github.com/docbox-nz",
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Provida Ltd. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
-
-  plugins: [
-    [
-      "docusaurus-plugin-openapi-docs",
-      {
-        id: "api", // plugin id
-        docsPluginId: "classic", // configured for preset-classic
-        config: {
-          docbox: {
-            downloadUrl:
-              "https://raw.githubusercontent.com/docbox-nz/docbox-website/refs/heads/main/api/docbox.json",
-            specPath: "api/docbox.json",
-            outputDir: "docs/api",
-            sidebarOptions: {
-              groupPathsBy: "tag",
-              categoryLinkSource: "tag",
-            },
-          } satisfies OpenApiPlugin.Options,
-        },
-      },
+                docs: {
+                    sidebarPath: "./sidebars.ts",
+                    docItemComponent: "@theme/ApiItem", // Derived from docusaurus-theme-openapi
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ["rss", "atom"],
+                        xslt: true,
+                    },
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    // Useful options to enforce blogging best practices
+                    onInlineTags: "warn",
+                    onInlineAuthors: "warn",
+                    onUntruncatedBlogPosts: "warn",
+                },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
-  themes: ["docusaurus-theme-openapi-docs"], // export theme components
+
+    themeConfig: {
+        colorMode: {
+            defaultMode: "dark",
+        },
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+            logo: {
+                alt: "Docbox",
+                src: "logo-dark.svg",
+                srcDark: "logo-light.svg",
+                width: 120,
+            },
+            items: [
+                {
+                    type: "docSidebar",
+                    sidebarId: "tutorialSidebar",
+                    position: "left",
+                    label: "Docs",
+                },
+                {
+                    type: "docSidebar",
+                    sidebarId: "apiSidebar",
+                    position: "left",
+                    label: "API",
+                },
+                {
+                    href: "https://github.com/docbox-nz",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
+        },
+        footer: {
+            style: "dark",
+            links: [
+                {
+                    title: "Docs",
+                    items: [
+                        {
+                            label: "Tutorial",
+                            to: "/docs/guides/intro",
+                        },
+                    ],
+                },
+                {
+                    title: "More",
+                    items: [
+                        {
+                            label: "GitHub",
+                            href: "https://github.com/docbox-nz",
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Provida Ltd. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+
+    plugins: [
+        [
+            "docusaurus-plugin-openapi-docs",
+            {
+                id: "api", // plugin id
+                docsPluginId: "classic", // configured for preset-classic
+                config: {
+                    docbox: {
+                        downloadUrl:
+                            "https://raw.githubusercontent.com/docbox-nz/docbox-website/refs/heads/main/api/docbox.json",
+                        specPath: "api/docbox.json",
+                        outputDir: "docs/api",
+                        sidebarOptions: {
+                            groupPathsBy: "tag",
+                            categoryLinkSource: "tag",
+                        },
+                    } satisfies OpenApiPlugin.Options,
+                },
+            },
+        ],
+    ],
+    themes: ["docusaurus-theme-openapi-docs"], // export theme components
 };
 
 export default config;
